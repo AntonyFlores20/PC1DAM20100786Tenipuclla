79f46091d2c5b0620aef9601b56573c4630517ba
nada <nada>
2024-10-05 09:33:09 -0500

    Initial commit

---
+++ app/src/main/java/dev/sebaspuruguay/pc1dam20100786tenipuclla/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package dev.sebaspuruguay.pc1dam20100786tenipuclla.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/dev/sebaspuruguay/pc1dam20100786tenipuclla/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package dev.sebaspuruguay.pc1dam20100786tenipuclla.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
